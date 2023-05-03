d609d3983ec49bee5b4cf30f6a1b3c383dbaf36a
annlaven <91900953+annlaven@users.noreply.github.com>
2023-05-03 08:56:26 +0300

    Update # Поиск в массиве заданного элемента.jl

--- # Поиск в массиве заданного элемента.jl
+++ # Поиск в массиве заданного элемента.jl
@@ -1,4 +1,17 @@
 # Поиск в массиве заданного элемента
+function quick_sort!(A)
+if isempty(A)
+return A
+end
+N = length(A)
+K, M = part_sort!(A, A[rand(1:N)]) # - "базовый" элемент массива выбирается
+случайным образом
+quick_sort!(@view A[1:K])
+quick_sort!(@view A[M:N])
+return A
+end
+#Базовй пример:
+
 array1 = [ 12, 35, 7, 34, 72, 12, 5 ]
 sch = 12
 
